81c5217f9f204127ac9ae561dc98e316f41e5c82
kdncode <kdncode@gmail.com>
2018-03-23 13:31:43 -0500

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController {
     var CorrectAnswer = String()
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
